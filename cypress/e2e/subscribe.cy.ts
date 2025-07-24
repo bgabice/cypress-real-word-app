4dae437372ffbe150abdf843d7fdef30edb9537f
bgabice <benjamingabice@gmail.com>
2025-07-24 15:29:12 +0200

    modified tests

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -15,16 +15,35 @@ describe("Newsletter Subscribe Form", () => {
       .and('contain.text', 'Success: gabicebenjamin@yahoo.com has been successfully subscribed');
   })
 
-it("does NOT allow an invalid email address", () => {
-  cy.getByData("email-input").type("invalidEmail")
-  cy.getByData("submit-button").click()
+    it("does NOT allow an invalid email address", () => {
+    cy.getByData("email-input").type("invalidEmail")
+    cy.getByData("submit-button").click()
 
-  cy.getByData("email-input").then(($input) => {
-    const inputEl = $input[0] as HTMLInputElement
-    const message = inputEl.validationMessage
+    cy.getByData("email-input").then(($input) => {
+        const inputEl = $input[0] as HTMLInputElement
+        const message = inputEl.validationMessage
 
-    expect(message).to.contain("Please include an '@' in the email address.");
-  })
-})
+        expect(message).to.contain("Please include an '@' in the email address.");
+    })
+    })
 
-})
+    it("shows an error message when the server returns an error", () => {
+      cy.intercept("POST", "/api/subscribe", {
+      statusCode: 500,
+      body: { message: "Error: Unable to subscribe at this time." }
+     }).as("subscribeError");
+
+  // Enter a valid email to allow the POST request to be made
+     cy.getByData("email-input").type("john@example.com");
+     cy.getByData("submit-button").click();
+
+  // Wait for the intercepted request to complete
+     cy.wait("@subscribeError");
+
+  // Assert that the error message is shown
+     cy.getByData("server-error-message")
+       .should("exist")
+       .and("contain.text", "Error: Unable to subscribe at this time.");
+});
+
+});
