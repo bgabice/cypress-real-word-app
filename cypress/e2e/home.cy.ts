b732be4ae2b2d75b91ce50f4800af1dee55d3f3b
bgabice <benjamingabice@gmail.com>
2025-07-24 12:47:56 +0200

    Added a context for test in hero section on homepage

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,9 +3,16 @@ describe('Homepage', () => {
     cy.visit('http://localhost:3000');
   });
 
-  it('has a hero section', () => {
-    cy.getByData('hero-heading')
-      .should('exist')
-      .contains('Testing Next.js Applications with Cypress');
+  context('Hero Section', () => {
+    it('has a hero section', () => {
+      cy.getByData('hero-heading')
+        .should('exist')
+        .contains('Testing Next.js Applications with Cypress');
+    });
+      it('has a list of sections', () => {
+      cy.get('dt').eq(0).should('contain','4 Courses');
+      cy.get('dt').eq(1).should('contain','25+ Lessons');
+      cy.get('dt').eq(2).should('contain','Free and Open Source');
+    });
   });
 });
